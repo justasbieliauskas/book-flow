ffeadc499277be77e2c3b8c8a1e447de25826efa
Justas Bieliauskas <justyvas0@gmail.com>
2019-07-08 20:40:53 +0300

    Updates

---
+++ precache-manifest.84fa65c901b3df1fe0ba499242555eec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ef5283221215c2d2651bffa90f67ade",
+    "url": "/book-flow/index.html"
+  },
+  {
+    "revision": "4bfd510ef87595e13024",
+    "url": "/book-flow/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "96bdd37a302bdbfed41d",
+    "url": "/book-flow/static/js/2.84c7e133.chunk.js"
+  },
+  {
+    "revision": "4bfd510ef87595e13024",
+    "url": "/book-flow/static/js/main.50f2d81e.chunk.js"
+  },
+  {
+    "revision": "030371ec1fbfd8369d11",
+    "url": "/book-flow/static/js/runtime~main.d2e0c97a.js"
+  }
+]);
