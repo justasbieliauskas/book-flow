6da07e384527222352f1995c53a2a246c6cc990b
Justas Bieliauskas <justyvas0@gmail.com>
2019-07-10 00:51:46 +0300

    Create <Books />

--- src/App.js
+++ src/App.js
@@ -4,11 +4,23 @@ import { HashRouter as Router, Route } from 'react-router-dom';
 import Books from './Books';
 
 export default class App extends Component {
-    render() {
-        return (
-            <Router>
-                <Route path="/" exact component={Books} />
-            </Router>
-        );
-    }
+  constructor() {
+    super();
+    this.books = [
+      {title: 'Code Ahead'},
+      {title: '12 Rules For Life'},
+    ];
+  }
+
+  render() {
+    return (
+      <Router>
+        <Route
+          path="/"
+          exact
+          render={() => <Books books={this.books} />}
+        />
+      </Router>
+    );
+  }
 }
--- src/Books.js
+++ src/Books.js
@@ -1,5 +1,14 @@
 import React, { Component } from 'react';
 
-export default function Books() {
-    return <h1>Books</h1>
+export default function Books(props) {
+  return (
+    <div>
+      <h1>Books</h1>
+      <ul>
+        {props.books.map(
+          ({title}) => <li>{title}</li>
+        )}
+      </ul>
+    </div>
+  );
 }
